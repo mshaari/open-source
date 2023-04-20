b80ef62be68bed4f62d0081e6171bb6266ad2170
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-20 16:01:38 -0400

    added styling and content to membership page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -12,15 +12,15 @@ function About(props) {
                 <p id='about-text'>
                     &lt;/Open Source&gt; is your one-stop-shop for tech job seekers!
                     <br></br>
-                    Here, you can search up tech jobs by job category, job location, 
+                    Here, you can sign up for free and search for tech jobs by category and location, 
                     <br></br>
-                    and save the job listings you're insterested in.
+                    and with our paid membership plan for just $15.00,
+                    <br></br> 
+                    youu'll get to save the job listings you're insterested in,
                     <br></br>
-                    With each job, you can even document your progress and make notes for ourself
+                    document your progress, and make notes for yourself,
                     <br></br>
-                    so you're always on top of your employment game.
-                    <br></br>
-                    Open Source is totally free!
+                    so you're always on top of your employment game!
                     <br></br>
                     Sign up and start your tech job hunting journey today!
                 </p>
--- client/src/components/pages/Memebership.js
+++ client/src/components/pages/Memebership.js
@@ -4,8 +4,24 @@ import '../../styles/pages.css';
 function Membership() {
     return (
         <div className='page-content'>
-            <div className='dashboard-container'>
-                <p className='Dashboard'>Dashboard</p>
+            <div className='membership-container membership-active'>
+                <h3 className='membership-title'>Paid Membership Plan</h3>
+                <p className='membership-text'>
+                    <span className='price'>A one-time payment of USD$15.00</span>
+                    <br></br>
+                    <span className='details'>
+                        Perks: 
+                        <br></br>
+                        save and edit your cover letter/ resume on your dashboard
+                        <br></br>
+                        save job listings to your dashboard
+                        <br></br>
+                        a checklist for each saved job listing to record your job hunting progress
+                        <br></br>
+                        make personal notes for yourself for each saved job listing
+                    </span>
+                </p>
+                <button id='checkout'>Pay Now</button>
             </div>
         </div>
 
