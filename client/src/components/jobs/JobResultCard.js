505daf949f8d509869fc598c58acdaf74b1b4a2e
mshaari <michael.shaari@gmail.com>
2023-04-21 19:54:33 -0400

    Updated data sent into jobs

--- client/src/components/jobs/JobResultCard.js
+++ client/src/components/jobs/JobResultCard.js
@@ -8,13 +8,16 @@ function JobResultCard(props) {
         <div className='projectList'>
             {jobs.map((job) => (
                 <div key={job.name} className='projectListItem'>
-                    <h3>{job.name} at {job.company}</h3>
+                    <a href={job.redirect_url}><h3>{job.title}</h3></a>
+                    <h5>{job.salary_predicted}</h5>
+                    <p>{job.description}</p>
                     <p>{job.contract_time}</p>
                     {/* <a href={job.url}><img src={process.env.PUBLIC_URL + '/assets/' + job.image} alt="Preview of this project" /></a>
                     <p className='flashingUrl'>View the <a href={job.url}>Deployed Application</a></p>
                     <p className='flashingUrl'>View the <a href={job.github}>GitHub Repository</a></p>
                     <p></p> */}
                     <button>Save This Job!</button>
+                    <p>----</p>
                 </div>
             ))}
         </div>
--- client/src/components/jobs/JobResults.js
+++ client/src/components/jobs/JobResults.js
@@ -6,14 +6,18 @@ function JobResults() {
     // Declare an array of objects that will get passed into Job element to create the multiple projects (array of objects with properties determined by what the API returns)
     const jobs = [
         {
-            company: "Google, Inc.",
-            name: "Senior Software Engineer",
-            contract_time: "Full Time"
+            title: "Google Senior Software Engineer",
+            description: "Work as a Software Engineer",
+            salary_predicted: 90000,
+            contract_time: "Full Time",
+            redirect_url: "https://careers.google.com/students/"
         },
         {
-            company: "Amazon",
-            name: "Junior Front-End Web Developer",
-            contract_time: "Part Time"
+            title: "Amazon Back End Developer",
+            description: "Work as a back end developer",
+            salary_predicted: 100000,
+            contract_time: "Part Time",
+            redirect_url: "https://www.amazon.com/"
         }
     ]
 
