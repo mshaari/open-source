19bddf4ae0189fde27f55a675997f320ff1db4b6
mshaari <michael.shaari@gmail.com>
2023-04-20 22:06:07 -0400

    Delete employers/seekers and fix job components

---
+++ client/src/components/jobs/JobResultCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../styles/pages.css';
+
+function JobResultCard(props) {
+    const { jobs } = props;
+
+    return (
+        <div className='projectList'>
+            {jobs.map((job) => (
+                <div key={job.name} className='projectListItem'>
+                    <a href={job.url}><h3>{job.name}</h3></a>
+                    <a href={job.url}><img src={process.env.PUBLIC_URL + '/assets/' + job.image} alt="Preview of this project" /></a>
+                    <p className='flashingUrl'>View the <a href={job.url}>Deployed Application</a></p>
+                    <p className='flashingUrl'>View the <a href={job.github}>GitHub Repository</a></p>
+                    <p></p>
+                    <button>Save This Job!</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default JobResultCard;
