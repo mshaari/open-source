0bdb2289bd96829e23ced88bfaa9d17833aa0b72
mshaari <michael.shaari@gmail.com>
2023-04-27 22:35:44 -0400

    Revert "Merge pull request #97 from mshaari/michael-apr27-heroku"
    This reverts commit cf990054ffc74f8a328de5b01c01594af5cd4dbe, reversing
    changes made to a7026d4966fd5dc23068657a1efab04ec96445f9.

--- client/src/components/common/Navigation.js
+++ client/src/components/common/Navigation.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import '../../styles/header.css';
 import { UserContext } from '../UserContext';
 import { redirect } from "react-router-dom";
@@ -34,31 +33,20 @@ function Navigation() {
     <div>
       {user.loggedIn ? (
         <nav className='navigation'>
-          <Link to={'/'} className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
-          {/* <a href="/" className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</a> */}
-          <Link to={"/login"} onClick={() => handleLogout()} className={currentPage === "Login" ? "nav-item-active" : "nav-item"}>Log Out</Link>
-          {/* <a href="/login" onClick={() => handleLogout()} className={currentPage === "Login" ? "nav-item-active" : "nav-item"}>Log Out</a> */}
-          <Link to={'/search'}className={currentPage === "/search" ? "nav-item-active" : "nav-item"}>Search</Link>
-          {/* <a href="/search" className={currentPage === "/search" ? "nav-item-active" : "nav-item"}>Search</a> */}
-          {/* <a href="/dashboard" className={currentPage === "/dashboard" ? "nav-item-active" : "nav-item"}>Dashboard</a>
+          <a href="/" className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</a>
+          <a href="/login" onClick={() => handleLogout()} className={currentPage === "Login" ? "nav-item-active" : "nav-item"}>Log Out</a>
+          <a href="/search" className={currentPage === "/search" ? "nav-item-active" : "nav-item"}>Search</a>
+          <a href="/dashboard" className={currentPage === "/dashboard" ? "nav-item-active" : "nav-item"}>Dashboard</a>
           {data.user.paid_member ? null : (
             <a href="/membership" className={currentPage === "/membership" ? "nav-item-active" : "nav-item"}>Membership</a>
-          )} */}
-          <Link to={"/dashboard"} className={currentPage === "/dashboard" ? "nav-item-active" : "nav-item"}>Dashboard</Link>
-          {data.user.paid_member ? null : (
-            <Link to={"/membership"} className={currentPage === "/membership" ? "nav-item-active" : "nav-item"}>Membership</Link>
           )}
-          <Link to='/contact' className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
-          {/* <a href="/contact" className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</a> */}
+          <a href="/contact" className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</a>
         </nav>
       ) : (
         <nav className='navigation'>
-          <Link to='/' className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
-          {/* <a href="/" className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</a> */}
-          <Link to='/login' className={currentPage === "/login" ? "nav-item-active" : "nav-item"}>Signup/Login</Link>
-          {/* <a href="/login" className={currentPage === "/login" ? "nav-item-active" : "nav-item"}>Signup/Login</a> */}
-          <Link to='/contact' className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
-          {/* <a href="/contact" className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</a> */}
+          <a href="/" className={currentPage === "/" ? "nav-item-active" : "nav-item"}>About Us</a>
+          <a href="/login" className={currentPage === "/login" ? "nav-item-active" : "nav-item"}>Signup/Login</a>
+          <a href="/contact" className={currentPage === "/contact" ? "nav-item-active" : "nav-item"}>Contact</a>
         </nav>
       )
       }
