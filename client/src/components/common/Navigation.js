11954fafbc800271c458edc02ced0ecec9867276
mshaari <michael.shaari@gmail.com>
2023-04-28 19:22:12 -0400

    Fixing pointer issue through UseEffect

--- client/src/components/common/Navigation.js
+++ client/src/components/common/Navigation.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../../styles/header.css';
 import { UserContext } from '../UserContext';
 import { redirect } from "react-router-dom";
@@ -10,13 +10,18 @@ function Navigation() {
 
   const [user, setUser] = useContext(UserContext);
 
-  let page = window.location.pathname;
-
   const { loading, error, data } = useQuery(QUERY_USER, {
     variables: { id: user.user_id },
     skip: !user.loggedIn,
   });
 
+  const [pathname, setPathname] = useState('/');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setPathname(location.pathname);
+  }, [location]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -35,21 +40,21 @@ function Navigation() {
   return (
     <div>
       {user.loggedIn ? (
-        <nav className='navigation' onClick={() => {page = window.location.pathname}}>
-          <Link to={'/'} className={page === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
-          <Link to={"/login"} onClick={() => handleLogout()} className={page === "/login" ? "nav-item-active" : "nav-item"}>Log Out</Link>
-          <Link to={'/search'} className={page === "/search" ? "nav-item-active" : "nav-item"}>Search</Link>
-          <Link to={"/dashboard"} className={page === "/dashboard" ? "nav-item-active" : "nav-item"}>Dashboard</Link>
+        <nav className='navigation' >
+          <Link to={'/'} className={pathname === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
+          <Link to={"/login"} onClick={() => handleLogout()} className={pathname === "/login" ? "nav-item-active" : "nav-item"}>Log Out</Link>
+          <Link to={'/search'} className={pathname === "/search" ? "nav-item-active" : "nav-item"}>Search</Link>
+          <Link to={"/dashboard"} className={pathname === "/dashboard" ? "nav-item-active" : "nav-item"}>Dashboard</Link>
           {data.user.paid_member ? null : (
-            <Link to="/membership" className={page === "/membership" ? "nav-item-active" : "nav-item"}>Membership</Link>
+            <Link to="/membership" className={pathname === "/membership" ? "nav-item-active" : "nav-item"}>Membership</Link>
           )}
-          <Link to='/contact' className={page === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
+          <Link to='/contact' className={pathname === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
         </nav>
       ) : (
-        <nav className='navigation' onClick={() =>page = window.location.pathname}>
-          <Link to='/' className={page === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
-          <Link to='/login' className={page === "/login" ? "nav-item-active" : "nav-item"}>Signup/Login</Link>
-          <Link to='/contact' className={page === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
+        <nav className='navigation'>
+          <Link to='/' className={pathname === "/" ? "nav-item-active" : "nav-item"}>About Us</Link>
+          <Link to='/login' className={pathname === "/login" ? "nav-item-active" : "nav-item"}>Signup/Login</Link>
+          <Link to='/contact' className={pathname === "/contact" ? "nav-item-active" : "nav-item"}>Contact</Link>
         </nav>
       )
       }
